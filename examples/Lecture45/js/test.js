d16b0a3b97052e22b0fadcbb26c810b622fe40c3
Jennifer Jiang2 <jiangbingqianj@gmail.com>
2018-04-23 23:15:55 -0400

    Literal notation

--- examples/Lecture45/js/test.js
+++ examples/Lecture45/js/test.js
@@ -1,14 +1,26 @@
-var company = new Object();
-company.name = 'google';
+// var company = new Object();
+// company.name = 'google';
 
-company.ceo = new Object();
-company.ceo.firstname = 'Sundar';
-company.ceo.lastname = 'Pichai';
+// company.ceo = new Object();
+// company.ceo.firstname = 'Sundar';
+// company.ceo.lastname = 'Pichai';
 
 
 
-var stock = '$stock of company';
-company[stock] = 110;
+// var stock = '$stock of company';
+// company[stock] = 110;
 
-company['stock'] = 110;
+// company['stock'] = 110;
+// console.log(company);
+
+//Literal Notation 
+var company = {
+	name: 'google', 
+	ceo: {
+		firstname: 'Sundar',
+		lastname: 'Pichai',
+	},
+	'$stock of company': 110,
+};
+
 console.log(company);
